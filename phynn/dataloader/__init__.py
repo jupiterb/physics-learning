11c530415a6ffece5afd3118909d8f1139834da6
jupiterb <bialyp26@gmail.com>
2024-04-12 16:53:09 +0200

    fix __init__ file of dataloader.  preprocessed_data/brain_tumor-progression_data notebook for 2d images

--- phynn/dataloader/__init__.py
+++ phynn/dataloader/__init__.py
@@ -3,4 +3,3 @@ from phynn.dataloader.hdf5 import HDF5DirectlyFromFile, HDF5LoadToMemory
 from phynn.dataloader.phyinformed import PhysicsInformedDataInterface
 from phynn.dataloader.shuffle import ShuffleDataInterface
 from phynn.dataloader.split import SplitDataInterface
-from phynn.dataloader.target_channel import TargetChannelDataInterface
